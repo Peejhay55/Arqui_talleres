5144f1aa8926e481d54cf70656e7291ecf3cac54
Pablo José Benítez Trujillo <beniteztrujillopablojose@gmail.com>
2026-08-17 17:59:48 -0500

    añadiendo tutorial 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,25 +10,13 @@ use Illuminate\Http\Request;
 
 use Illuminate\View\View; 
 
+use App\Models\Product;
+
  
 
 class ProductController extends Controller 
 
 { 
-
-    public static $products = [ 
-
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV"], 
-
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone"], 
-
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast"], 
-
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses"] 
-
-    ]; 
-
- 
 
     public function index(): View 
 
@@ -40,7 +28,7 @@ class ProductController extends Controller
 
         $viewData["subtitle"] =  "List of products"; 
 
-        $viewData["products"] = ProductController::$products; 
+        $viewData["products"] = Product::all(); 
 
         return view('product.index')->with("viewData", $viewData); 
 
@@ -54,7 +42,7 @@ class ProductController extends Controller
 
         $viewData = []; 
 
-        $product = ProductController::$products[$id-1]; 
+        $product = Product::findOrFail($id); 
 
         $viewData["title"] = $product["name"]." - Online Store"; 
 
